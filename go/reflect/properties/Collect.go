b8f1a5ec0724dd6d75ad8f17de7a733f31ced00d
Sharon Aicler <saichler@gmail.com>
2025-11-19 08:00:05 -0800

    Add propertyid to collect

--- go/reflect/properties/Collect.go
+++ go/reflect/properties/Collect.go
@@ -3,49 +3,56 @@ package properties
 import (
 	"reflect"
 
+	"github.com/saichler/l8reflect/go/reflect/helping"
 	"github.com/saichler/l8types/go/ifs"
 	"github.com/saichler/l8types/go/types/l8reflect"
 )
 
-func Collect(root interface{}, r ifs.IResources, typeName string) []interface{} {
+func Collect(root interface{}, r ifs.IResources, typeName string) map[string]interface{} {
 	typ := reflect.ValueOf(root).Elem().Type()
 	node, _ := r.Introspector().NodeByType(typ)
-	list := make([]interface{}, 0)
-	collect(root, node, typeName, &list)
-	return list
+	result := make(map[string]interface{}, 0)
+	rootKey := helping.PrimaryDecorator(node, reflect.ValueOf(root), r.Registry())
+	rootProp := NewProperty(node, nil, rootKey, root, r)
+	collect(root, node, typeName, rootProp, rootKey, result, r)
+	return result
 }
 
-func collect(any interface{}, node *l8reflect.L8Node, typeName string, list *[]interface{}) {
+func collect(any interface{}, node *l8reflect.L8Node, typeName string,
+	parent *Property, key interface{}, elems map[string]interface{}, r ifs.IResources) {
 	val := reflect.ValueOf(any)
-	if val.Kind() != reflect.Ptr {
+	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
-	typ := val.Elem().Type()
+	typ := val.Type()
+	myProperty := NewProperty(node, parent, key, any, r)
 	if typ.Name() == typeName {
-		*list = append(*list, any)
+		id, _ := myProperty.PropertyId()
+		elems[id] = any
 		return
 	}
+
 	if node.Attributes != nil {
 		for _, attr := range node.Attributes {
-			if attr.IsStruct {
+			if attr.IsMap {
 				value := val.FieldByName(attr.FieldName)
 				if value.IsValid() {
-					collect(value.Interface(), attr, attr.TypeName, list)
+					keys := value.MapKeys()
+					for i := 0; i < len(keys); i++ {
+						collect(value.MapIndex(keys[i]).Interface(), attr, typeName, myProperty, keys[i].Interface(), elems, r)
+					}
 				}
 			} else if attr.IsSlice {
 				value := val.FieldByName(attr.FieldName)
 				if value.IsValid() {
 					for i := 0; i < value.Len(); i++ {
-						collect(value.Index(i).Interface(), attr, attr.TypeName, list)
+						collect(value.Index(i).Interface(), attr, typeName, myProperty, i, elems, r)
 					}
 				}
-			} else if attr.IsMap {
+			} else if attr.IsStruct {
 				value := val.FieldByName(attr.FieldName)
 				if value.IsValid() {
-					keys := value.MapKeys()
-					for i := 0; i < len(keys); i++ {
-						collect(value.MapIndex(keys[i]).Interface(), attr, attr.TypeName, list)
-					}
+					collect(value.Interface(), attr, typeName, myProperty, nil, elems, r)
 				}
 			}
 		}
